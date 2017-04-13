b8500105551f1150ab1d95f0227047de8683dcc4
Jean Carlo Nascimento <jnascimento@gmail.com>
2017-04-13 20:30:18 -0300

    Update bitcoinValue.js

--- Previsao/bitcoinValue.js
+++ Previsao/bitcoinValue.js
@@ -1,14 +1,16 @@
-const bitcoinValue  = function(compra, porcento, real){
+const calcTotalComRetencao = (totalComRetencao, porcento) => 
+  (totalComRetencao*porcento).toFixed(8)
+
+const bitcoinValue  = (compra, porcento, real) => {
     // Porcento ser colocado a porcentagem dividida por 100
-    totalSemRetencao = compra/real
-    totalComRetencao = totalSemRetencao
-    totalComRetencao -= totalComRetencao*porcento
-    totalComRetencao = totalComRetencao.toFixed(8)
-    totalSemRetencao = totalSemRetencao.toFixed(8)
+    const totalSemRetencao = compra/real
+    const totalComRetencao = totalSemRetencao
+    const totalComRetencaoFinal = calcTotalComRetencao(totalComRetencao, porcento)
+    const totalSemRetencaoFinal = totalSemRetencao.toFixed(8)
     console.log("Comprou um total de: " + totalSemRetencao +
                 "\nCom a retenção do site o valor total foi de: " + totalComRetencao)
 
-    return totalComRetencao
+    return totalComRetencaoFinal
 }
 console.log(bitcoinValue(500, 0.005, 3601.02))
 module.exports = bitcoinValue
